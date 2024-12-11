3477cfb88dbbf9173a966e438d550524496d9282
chandangupta19 <chandan.cg11@gmail.com>
2024-12-11 07:56:41 +0530

    Update Unburden.tsx

--- src/Unburden.tsx
+++ src/Unburden.tsx
@@ -9,7 +9,7 @@ import {
   AlertDialogFooter,
   AlertDialogAction,
 } from './components/ui/alert-dialog';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const MAX_CHARACTERS = 5000;
 const MIN_SLIDE_THRESHOLD = 90;
